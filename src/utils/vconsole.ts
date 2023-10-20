db3127bd6474bd7fb0e744367ff0693bbbe52749
whevether <whevether@outlook.com>
2023-10-20 18:02:19 +0800

    update

--- src/utils/vconsole.ts
+++ src/utils/vconsole.ts
@@ -2,7 +2,7 @@ import { config } from '../config'
 import { isEnvDevelopment } from './env'
 import {JsBridge} from '@/utils/Jsbridge'
 let vConsole = {}
-if ((isEnvDevelopment || JsBridge('debug')) && config.vConsole) {
+if ((isEnvDevelopment && !JsBridge('debug')) && config.vConsole) {
   import('vconsole').then(({ default: VConsole }) => {
     vConsole = new VConsole({ theme: 'dark' })
   })
